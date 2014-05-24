d60240da82d5b6f2932325b77543fb2af8eb555f
jayaskren <jay.askren@gmail.com>
2014-05-24 17:27:47 -0600

    Committing NIO version

---
+++ src/main/java/com/fastopencsv/AbstractCsvReader.java
@@ -0,0 +1,232 @@
+package com.fastopencsv;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractCsvReader implements AutoCloseable {
+
+	protected int bufferSize = 1024*1024;
+	protected boolean hasNext = true;
+	protected CSVParser parser;
+	protected int skipLines;
+	protected boolean linesSkiped;
+	protected char[] buffer;
+	protected int positionInBuffer = -1;
+	protected long totalCharactersRead = 0;
+	protected int charactersRead;
+	protected char[] nextLine;
+	/**
+	 * The default line to start reading.
+	 */
+	public static final int DEFAULT_SKIP_LINES = 0;
+
+	public static final int[] getNextLineBoundaries(int start, int end,
+			char[] characters) {
+			    	int[] returnValue = new int[]{-1,-1};
+			    	final int length = Math.min(characters.length, end);
+			    	for (int i=start; i< length; i++) {
+			//    		System.out.println("boundaries " + characters);
+			    		if (characters[i] == '\n' || characters[i] == '\r') {
+			    			int previousPos = i-1;
+			    			if (returnValue[0] >= 0 && previousPos >= returnValue[0] ) {
+			    				 // We have already set the first position.  Now we have reached the end of this line.
+			    				 // Set the second position
+			    				returnValue[1] = previousPos; // TODO
+			    				break;
+			    			 } else {
+			    				 returnValue[1] = i;
+			    				 returnValue[0] = i;
+			    				 break;
+			    			 }
+			    			
+						} else {
+							if (returnValue[0] < 0) {
+								// We have not set the first position yet and we see a non new 
+								//   line character.  This is the first character of the line.
+			    				returnValue[0] = i;
+			    			}
+						}
+			    	}
+			
+			    	return returnValue;
+			    }
+
+	/**
+	     * Reads the entire file into a List with each element being a String[] of
+	     * tokens.
+	     * 
+	     * @return a List of String[], with each String[] representing a line of the
+	     *         file.
+	     * 
+	     * @throws IOException
+	     *             if bad things happen during the read
+	     */
+	public List<String[]> readAll() throws IOException {
+	
+	    List<String[]> allElements = new ArrayList<String[]>();
+	    while (hasNext) {
+	        String[] nextLineAsTokens = readNext();
+	        if (nextLineAsTokens != null)
+	            allElements.add(nextLineAsTokens);
+	    }
+	    return allElements;
+	
+	}
+
+	/**
+	 * Reads the next line from the buffer and converts to a string array.
+	 * 
+	 * @return a string array with each comma-separated element as a separate
+	 *         entry.
+	 * 
+	 * @throws IOException
+	 *             if bad things happen during the read
+	 */
+	public final String[] readNext() throws IOException {
+		String[] result = null;
+		do {
+			nextLine = getNextLine();
+			if (!hasNext) {
+				return result; // should throw if still pending?
+			}
+			String[] r = parser.parseLineMulti(nextLine);
+			if (r.length > 0) {
+				if (result == null) {
+					result = r;
+				} else {
+					String[] t = new String[result.length+r.length];
+					System.arraycopy(result, 0, t, 0, result.length);
+					System.arraycopy(r, 0, t, result.length, r.length);
+					result = t;
+				}
+			}
+		} while (parser.isPending());
+		return result;
+	}
+
+	/**
+	 * Reads the next line from the file.
+	 * 
+	 * @return the next line from the file without trailing newline
+	 * @throws IOException
+	 *             if bad things happen during the read
+	 */
+	private final char[] getNextLine() throws IOException {
+		if (!this.linesSkiped) {
+	        for (int i = 0; i < skipLines; i++) {
+	            readLine();
+	        }
+	        this.linesSkiped = true;
+	    }
+	    char[] nextLine = readLine();
+	    if (nextLine == null) {
+	        hasNext = false;
+	    }
+	    return hasNext ? nextLine : null;
+	}
+
+	public final char[] getNextLineText() {
+		return nextLine;
+	}
+
+	protected final char[] readLine() throws IOException {
+	    	boolean foundLine = false;
+	    	char[] returnLine = null;
+	    	char[] partialLine = null;
+	    	
+	    	while (!foundLine) {
+	//    		System.out.println("readline " + positionInBuffer + " " + new String(buffer));
+	    		if (positionInBuffer < 0 || positionInBuffer >= buffer.length) {
+	    			// read more data
+	    			charactersRead = read(); 
+	    			
+	        		totalCharactersRead += charactersRead;
+	        		positionInBuffer = 0;
+	    		}
+	    		
+	    		int[] lineBoundaries = getNextLineBoundaries(positionInBuffer, charactersRead, buffer);
+	    		int lengthOfLine;
+	    		if (lineBoundaries[0] < 0 && lineBoundaries[1] < 0) {
+	    			// We reached the end of the buffer and did not find the a new line.
+	    			// reset positionInBuffer
+	    			// Continue and read in more data
+	    			if (charactersRead < 0) {
+	    				return partialLine;
+	    			}
+	    			positionInBuffer = -1;
+	    			continue;
+	    		} else if (lineBoundaries[1] >= 0) {
+	    			// Newline found
+	    			lengthOfLine = lineBoundaries[1] - Math.max(0, lineBoundaries[0])+1;
+	    		} else {
+	    			lengthOfLine = charactersRead - lineBoundaries[0];
+	    		}
+	    		
+	    		if (charactersRead < 0 ) {
+	    			// We've reached the end of the file
+	    			return partialLine;
+	    		} else if (lineBoundaries[0]>=0) {
+	    			if  (lineBoundaries[1] < 0) {
+	    				// If partialLine is already created, just add to it.
+	    				if (partialLine == null) {
+	    					partialLine = new char[lengthOfLine];
+	    					System.arraycopy(buffer, lineBoundaries[0], partialLine, 0, lengthOfLine);
+	    				} else {
+	    					char[] newPartialLine = new char[lengthOfLine + partialLine.length];
+	    					System.arraycopy(partialLine, 0, newPartialLine, 0, partialLine.length);
+	    					System.arraycopy(buffer, lineBoundaries[0], newPartialLine, partialLine.length, lengthOfLine);
+	    					partialLine = newPartialLine;
+	    				}
+	    				positionInBuffer = -1;
+	    			} else if (lineBoundaries[0] == lineBoundaries[1] && (buffer[lineBoundaries[0]] == '\n' || buffer[lineBoundaries[0]] == '\r')){
+	    				//    Found a new line.  If there is a partial line, return it otherwise keep looking.
+						// Todo put a better check than this
+	    				
+	    				positionInBuffer = lineBoundaries[1]+2;
+	    				if(partialLine != null) {
+	    					return partialLine;
+	    				}
+	
+	    			} else {
+	    				if (partialLine == null) {
+	    					returnLine = new char[lengthOfLine];
+	    					System.arraycopy(buffer, lineBoundaries[0], returnLine, 0, lengthOfLine);
+	    				} else {
+	    					returnLine = new char[lengthOfLine + partialLine.length];
+	    					System.arraycopy(partialLine, 0, returnLine, 0, partialLine.length);
+	    					System.arraycopy(buffer, lineBoundaries[0], returnLine, partialLine.length, lengthOfLine);
+	    					partialLine = null;
+	    				}
+	    				positionInBuffer = lineBoundaries[1]+2;
+	    				return returnLine;
+	    			}
+	    		} else {
+	    			// Keep looping.  The only case where I can think of that this would happen is if  
+	    			//  the buffer is full of new line characters.  Hopefully that will never happen.
+	    		}
+	
+	    	}
+	    	return returnLine;
+	    }
+
+	public AbstractCsvReader() {
+		super();
+	}
+
+	public final long getTotalCharactersRead() {
+		return totalCharactersRead;
+	}
+
+	public abstract int read() throws IOException ;
+	
+	/**
+	 * Closes the underlying reader.
+	 * 
+	 * @throws IOException if the close fails
+	 */
+	public abstract void close() throws IOException ;
+
+}
---
+++ src/main/java/com/fastopencsv/CsvNioReader.java
@@ -0,0 +1,205 @@
+package com.fastopencsv;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+public class CsvNioReader extends AbstractCsvReader {
+	
+	protected FileChannel channel;
+	protected RandomAccessFile file;
+	protected ByteBuffer byteBuffer;
+	
+    /**
+     * Constructs CSVReader using a comma for the separator.
+     * 
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @throws IOException 
+     */
+    public CsvNioReader(File file)  throws IOException {
+        this(file, CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_QUOTE_CHARACTER, CSVParser.DEFAULT_ESCAPE_CHARACTER);
+    }
+
+    /**
+     * Constructs CSVReader with supplied separator.
+     * 
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries.
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator)  throws IOException {
+        this(file, separator, CSVParser.DEFAULT_QUOTE_CHARACTER, CSVParser.DEFAULT_ESCAPE_CHARACTER);
+    }
+
+    /**
+     * Constructs CSVReader with supplied separator and quote char.
+     * 
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar)  throws IOException {
+        this(file, separator, quotechar, CSVParser.DEFAULT_ESCAPE_CHARACTER, DEFAULT_SKIP_LINES, CSVParser.DEFAULT_STRICT_QUOTES);
+    }
+
+    /**
+     * Constructs CSVReader with supplied separator, quote char and quote handling
+     * behavior.
+     *
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param strictQuotes
+     *            sets if characters outside the quotes are ignored
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar, boolean strictQuotes)  throws IOException {
+        this(file, separator, quotechar, CSVParser.DEFAULT_ESCAPE_CHARACTER, DEFAULT_SKIP_LINES, strictQuotes);
+    }
+
+   /**
+     * Constructs CSVReader with supplied separator and quote char.
+     *
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param escape
+     *            the character to use for escaping a separator or quote
+ * @throws IOException 
+     */
+
+    public CsvNioReader(File file, char separator, char quotechar, char escape)  throws IOException {
+        this(file, separator, quotechar, escape, DEFAULT_SKIP_LINES, CSVParser.DEFAULT_STRICT_QUOTES);
+	}
+    
+    /**
+     * Constructs CSVReader with supplied separator and quote char.
+     * 
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param line
+     *            the line number to skip for start reading 
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar, int line)  throws IOException {
+        this(file, separator, quotechar, CSVParser.DEFAULT_ESCAPE_CHARACTER, line, CSVParser.DEFAULT_STRICT_QUOTES);
+    }
+
+    /**
+     * Constructs CSVReader with supplied separator and quote char.
+     *
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param escape
+     *            the character to use for escaping a separator or quote
+     * @param line
+     *            the line number to skip for start reading
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar, char escape, int line) throws IOException {
+        this(file, separator, quotechar, escape, line, CSVParser.DEFAULT_STRICT_QUOTES);
+    }
+    
+    /**
+     * Constructs CSVReader with supplied separator and quote char.
+     * 
+     * @param reader
+     *            the reader to an underlying CSV source.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param escape
+     *            the character to use for escaping a separator or quote
+     * @param line
+     *            the line number to skip for start reading
+     * @param strictQuotes
+     *            sets if characters outside the quotes are ignored
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar, char escape, int line, boolean strictQuotes)  throws IOException {
+        this(file, separator, quotechar, escape, line, strictQuotes, CSVParser.DEFAULT_IGNORE_LEADING_WHITESPACE);
+    }
+
+    /**
+     * Constructs CSVReader with supplied separator and quote char.
+     * @param separator
+     *            the delimiter to use for separating entries
+     * @param quotechar
+     *            the character to use for quoted elements
+     * @param escape
+     *            the character to use for escaping a separator or quote
+     * @param line
+     *            the line number to skip for start reading
+     * @param strictQuotes
+     *            sets if characters outside the quotes are ignored
+     * @param ignoreLeadingWhiteSpace
+     *            it true, parser should ignore white space before a quote in a field
+     * @throws IOException 
+     */
+    public CsvNioReader(File file, char separator, char quotechar, char escape, int line, boolean strictQuotes, boolean ignoreLeadingWhiteSpace) throws IOException {
+        this.parser = new CSVParser(separator, quotechar, escape, strictQuotes, ignoreLeadingWhiteSpace);
+        this.skipLines = line;
+        this.file = new RandomAccessFile(file, "r");
+        if (file != null) {
+        	channel = this.file.getChannel();
+        }
+        
+        byteBuffer = ByteBuffer.allocate(this.bufferSize);
+        totalCharactersRead = charactersRead = read();
+        positionInBuffer = 0;
+    }
+
+	@Override
+	public final int read() throws IOException {
+		int count = channel.read(byteBuffer);
+		if (count < 0) {
+			return count;
+		}
+		
+		byteBuffer.flip();
+		CharBuffer charBuf = StandardCharsets.UTF_8.decode(byteBuffer);
+		if (buffer == null) {
+			buffer = new char[charBuf.remaining()];
+		}
+		int i = 0;
+		while (charBuf.hasRemaining()) {
+			this.buffer[i] = charBuf.get();
+			i++;
+		}
+		byteBuffer.clear();
+		return i;
+	}
+
+	@Override
+	public final void close() throws IOException {
+		file.close();
+	}
+
+}
